23008942dde11073b7e563a50063085cfde5d827
Vladimir Markelov <vsmarkelov@yandex.ru>
2025-07-15 13:20:19 +0300

    fix cypress test

--- cypress/e2e/constructor.cy.js
+++ cypress/e2e/constructor.cy.js
@@ -4,7 +4,9 @@ const BASE_URL = 'http://localhost:4000';
 
 describe('Burger constructor flow', () => {
   beforeEach(() => {
-    cy.intercept('GET', '**/ingredients', { fixture: 'ingredients.json' }).as('getIngredients');
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
     cy.visit(BASE_URL);
     cy.wait('@getIngredients');
   });
@@ -21,7 +23,7 @@ describe('Burger constructor flow', () => {
   it('opens and closes ingredient modal', () => {
     cy.contains('li', 'Флюоресцентная булка R2-D3').find('a').click();
     cy.contains('h3', 'Детали ингредиента').should('exist');
-    cy.get('button').contains('×').click({ force: true });
+    cy.get('#modals').find('button').first().click();
     cy.contains('h3', 'Детали ингредиента').should('not.exist');
   });
 
@@ -29,8 +31,12 @@ describe('Burger constructor flow', () => {
     cy.setCookie('accessToken', 'test');
     cy.window().then((win) => win.localStorage.setItem('refreshToken', 'test'));
 
-    cy.intercept('GET', '**/auth/user', { fixture: 'user.json' });
+    cy.intercept('GET', '**/api/auth/user', { fixture: 'user.json' }).as('getUser');
     cy.intercept('POST', '**/orders', { fixture: 'order.json' }).as('createOrder');
+
+    cy.reload();
+    cy.wait('@getIngredients');
+    cy.wait('@getUser');
 
     cy.contains('li', 'Флюоресцентная булка R2-D3').find('button').click();
     cy.contains('li', 'Сыр с астероидной плесенью').find('button').click();
@@ -39,7 +45,7 @@ describe('Burger constructor flow', () => {
     cy.wait('@createOrder');
 
     cy.contains('h2', '1991').should('exist');
-    cy.get('button').contains('×').click({ force: true });
+    cy.get('#modals').find('button').first().click();
 
     cy.contains('section', 'Оформить заказ').should('not.contain', 'Сыр с астероидной плесенью');
   });
