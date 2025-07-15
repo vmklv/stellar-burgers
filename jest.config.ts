da89d63c1a989e6144f7d18854d8f921e5c6af1c
Vladimir Markelov <vsmarkelov@yandex.ru>
2025-07-15 12:41:08 +0300

    fix jest tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import { compilerOptions } from './tsconfig.json';
 
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '\\.(css)$': 'identity-obj-proxy'
+    '\\.(css)$': 'identity-obj-proxy',
+    ...pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' })
   },
   collectCoverage: true
 };
